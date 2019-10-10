c32ca45c50760603c23ad554be408d71937bc2b9
RichardT74 <richtrist@gmail.com>
2019-10-10 10:48:45 +0100

    one more questions to do

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,14 +16,18 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
-//test 1
+//test 1 running total
   it('should have the number buttons update the display of the running total', function(){
     element(by.css('#number5')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('5');
   })
 
+// test 2 arithmical operations
 
-//test 2
+
+
+
+//test 3 multiple operations
 
 it('should chain multiple operations together', function(){
    running_total = element(by.css('#running_total'))
@@ -36,7 +40,7 @@ it('should chain multiple operations together', function(){
    expect(running_total.getAttribute('value')).to.eventually.equal('50')
  });
 
-//test 3
+//test 4.1 range of numbers
  it('should handle negative output', function() {
    running_total = element(by.css('#running_total'))
    element(by.css('#number5')).click();
@@ -46,4 +50,29 @@ it('should chain multiple operations together', function(){
    expect(running_total.getAttribute('value')).to.eventually.equal('-4')
  });
 
+ //test 4.2 range of numbers large output
+ it('should handle negative output', function() {
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number9')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#number9')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number8')).click();
+  element(by.css('#number8')).click();
+  element(by.css('#number8')).click();
+  element(by.css('#operator_multiply')).click();
+  element(by.css('#number1')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('8871120')
+})
+
+//test 5
+it('should return "You can\'t divide by zero" when the user tries to divide by zero', function(){
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('You can\'t divide by zero')
+  })
 });
